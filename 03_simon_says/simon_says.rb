777e7b6f1b313f92667c0c466060bd0df1a28689
Asel-K <asel.karagazieva@gmail.com>
2022-04-21 00:52:18 -0500

    Ruby Test

--- 03_simon_says/simon_says.rb
+++ 03_simon_says/simon_says.rb
@@ -1,1 +1,28 @@
 #write your code here
+def echo(greeting)
+    greeting 
+end 
+
+def shout(shouting)
+    shouting.upcase
+end 
+
+def repeat(word, number=2)
+    repeat_word = (word + " ") * number
+    repeat_word.rstrip!
+end
+
+def start_of_word(word, num)
+    word[0..(num-1)]
+    
+end 
+
+def first_word(string)
+    string.split[0]
+end 
+
+def titleize(string)
+    # string.split.map(&:capitalize).join(' ')
+    lowercase_words = %w{a an the over and but or for nor of}
+    string.split.each_with_index.map{|x, index| lowercase_words.include?(x) && index > 0 ? x : x.capitalize }.join(" ")
+end 
--- 04_pig_latin/pig_latin.rb
+++ 04_pig_latin/pig_latin.rb
@@ -1,1 +1,12 @@
 #write your code here
+def translate (word)
+    vowels = %w{a e i o u}
+    word.gsub(/(\A|\s)\w+/) do |str|
+            str.strip!
+        while not vowels.include? str[0] or (str[0] == 'u' and str[-1] == 'q')
+            str += str[0]
+            str = str[1..-1]
+        end
+        str  = ' ' + str + 'ay'
+    end.strip
+end
